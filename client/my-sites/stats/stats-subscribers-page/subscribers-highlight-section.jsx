04c375c9970aa22a639823610831b4eb5d78f850
Kevin L <40267301+a8ck3n@users.noreply.github.com>
2023-05-01 14:09:59 +0800

    First pass as refactoring the section header.
    An example of encapsulating the header and popover handling.

--- client/my-sites/stats/stats-subscribers-page/subscribers-highlight-section.jsx
+++ client/my-sites/stats/stats-subscribers-page/subscribers-highlight-section.jsx
@@ -31,6 +31,53 @@ function useSubscriberHighlights() {
 	return highlights;
 }
 
+function BasicSectionHeaderInfoPopover( props ) {
+	const [ isTooltipVisible, setTooltipVisible ] = useState( false );
+	const infoReferenceElement = useRef( null );
+	return (
+		<>
+			<span
+				className="info-wrapper"
+				ref={ infoReferenceElement }
+				onMouseEnter={ () => setTooltipVisible( true ) }
+				onMouseLeave={ () => setTooltipVisible( false ) }
+			>
+				<Icon className="info-icon" icon={ info } />
+			</span>
+			<Popover
+				className="tooltip tooltip--darker tooltip-wordads highlight-card-tooltip"
+				isVisible={ isTooltipVisible }
+				position="bottom right"
+				context={ infoReferenceElement.current }
+			>
+				<div className="highlight-card-tooltip-content">{ props.popoverContent }</div>
+			</Popover>
+		</>
+	);
+}
+
+function BasicSectionHeader( props ) {
+	// Values coming from caller (via props).
+	const title = props.title;
+	const popoverContent = props.popoverContent;
+	const subTitle = props.subTitle;
+	const subTitleLink = props.subTitleLink;
+	// Set up popover.
+	let popover = null;
+	if ( popoverContent !== undefined ) {
+		popover = <BasicSectionHeaderInfoPopover popoverContent={ popoverContent } />;
+	}
+	// Do the same for a subtitle with optional link. Should be either/or.
+	// Update the JSX as needed.
+	return (
+		<div className="highlight-cards">
+			<h1 className="highlight-cards-heading">
+				{ title } { popover }
+			</h1>
+		</div>
+	);
+}
+
 function SubscriberHighlightsHeader() {
 	const translate = useTranslate();
 	const [ isTooltipVisible, setTooltipVisible ] = useState( false );
@@ -82,10 +129,13 @@ function SubscriberHighlightsListing() {
 }
 
 export default function SubscribersHighlightSection() {
+	let popoverContent = <p>Popover content goes here.</p>;
 	return (
 		<div className="highlight-cards subscribers-page has-odyssey-stats-bg-color">
 			<SubscriberHighlightsHeader />
 			<SubscriberHighlightsListing />
+			<BasicSectionHeader title="New section header" popoverContent={ popoverContent } />
+			<BasicSectionHeader title="New section header without popover" />
 		</div>
 	);
 }
