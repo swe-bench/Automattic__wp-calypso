fd2900283d0744e8caee51c1d5cd94ccdaac522f
Aneesh Devasthale <aneesh.devasthale@automattic.com>
2024-08-09 16:25:17 +0530

    deemphasize free plan

--- client/signup/config/steps-pure.js
+++ client/signup/config/steps-pure.js
@@ -107,6 +107,7 @@ export function generateSteps( {
 			optionalDependencies: [ 'themeSlugWithRepo' ],
 			props: {
 				intent: 'plans-site-selected-legacy',
+				deemphasizeFreePlan: true,
 			},
 		},
 
