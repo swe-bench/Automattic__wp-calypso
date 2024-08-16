85db9fe3554705c2e8cf3a1b10f455ea52f24c14
heavyweight <kpapazov@gmail.com>
2024-08-16 13:11:56 +0200

    Add tailored section mapping

--- packages/help-center/src/components/help-center-search-results.tsx
+++ packages/help-center/src/components/help-center-search-results.tsx
@@ -200,17 +200,14 @@ function HelpSearchResults( {
 		filterManagePurchaseLink( hasPurchases, isPurchasesSection )
 	);
 
-	const { contextSearch, tailoredArticles } = useContextBasedSearchMapping( currentRoute, locale );
+	const { contextSearch, tailoredSection } = useContextBasedSearchMapping( currentRoute );
 
 	const [ debouncedQuery ] = useDebounce( searchQuery || '', 500 );
 
 	const { data: searchData, isLoading: isSearching } = useHelpSearchQuery(
 		debouncedQuery || contextSearch, // If there's a query, we don't context search
 		locale,
-		sectionName,
-		debouncedQuery
-			? undefined // If there's a query, we don't need tailored articles
-			: tailoredArticles
+		tailoredSection || sectionName
 	);
 
 	const searchResults = searchData ?? [];
@@ -272,9 +269,10 @@ function HelpSearchResults( {
 			section: sectionName,
 		};
 
-		const eventName = tailoredArticles?.post_ids.includes( post_id ?? 0 )
-			? 'calypso_inlinehelp_tailored_article_select'
-			: 'calypso_inlinehelp_article_select';
+		const eventName =
+			tailoredSection && ! searchQuery
+				? 'calypso_inlinehelp_tailored_article_select'
+				: 'calypso_inlinehelp_article_select';
 
 		recordTracksEvent( eventName, eventData );
 		onSelect( event, result );
