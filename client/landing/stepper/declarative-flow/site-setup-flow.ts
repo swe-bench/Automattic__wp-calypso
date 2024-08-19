d812fbd4e519f821173ca9a7e5602bd05e9fd514
arthur <arthur.chu@automattic.com>
2024-08-19 13:50:18 +0800

    Fix design choices step when the big sky is enabled

--- client/landing/stepper/declarative-flow/site-setup-flow.ts
+++ client/landing/stepper/declarative-flow/site-setup-flow.ts
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 import wpcomRequest from 'wpcom-proxy-request';
 import { isTargetSitePlanCompatible } from 'calypso/blocks/importer/util';
 import { useIsSiteAssemblerEnabledExp } from 'calypso/data/site-assembler';
+import { useIsBigSkyEligible } from 'calypso/landing/stepper/hooks/use-is-site-big-sky-eligible';
 import { useQuery } from 'calypso/landing/stepper/hooks/use-query';
 import { ImporterMainPlatform } from 'calypso/lib/importer/types';
 import { addQueryArgs } from 'calypso/lib/route';
@@ -151,6 +152,12 @@ const siteSetupFlow: Flow = {
 		);
 
 		const isSiteAssemblerEnabled = useIsSiteAssemblerEnabledExp( 'design-choices' );
+
+		const { isEligible: isBigSkyEligible } = useIsBigSkyEligible();
+
+		const isDesignChoicesStepEnabled =
+			isEnabled( 'onboarding/design-choices' ) &&
+			( ( isAssemblerSupported() && isSiteAssemblerEnabled ) || isBigSkyEligible );
 
 		const { setPendingAction, resetOnboardStoreWithSkipFlags, setIntent } =
 			useDispatch( ONBOARD_STORE );
@@ -357,11 +364,7 @@ const siteSetupFlow: Flow = {
 						case SiteIntent.Sell:
 							return navigate( 'options' );
 						default: {
-							if (
-								isEnabled( 'onboarding/design-choices' ) &&
-								isAssemblerSupported() &&
-								isSiteAssemblerEnabled
-							) {
+							if ( isDesignChoicesStepEnabled ) {
 								return navigate( 'design-choices' );
 							}
 							return navigate( 'designSetup' );
@@ -504,11 +507,7 @@ const siteSetupFlow: Flow = {
 						case SiteIntent.Write:
 							return navigate( 'bloggerStartingPoint' );
 						default: {
-							if (
-								isEnabled( 'onboarding/design-choices' ) &&
-								isAssemblerSupported() &&
-								isSiteAssemblerEnabled
-							) {
+							if ( isDesignChoicesStepEnabled ) {
 								return navigate( 'design-choices' );
 							}
 							return navigate( 'goals' );
